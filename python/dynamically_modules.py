27f3b5bb174e6aeda4fbdc500549f7920280cc0a
david <david.iwui@gmail.com>
2020-02-01 10:11:24 +0800

    use globals to add modules

--- python/dynamically_modules.py
+++ python/dynamically_modules.py
@@ -8,11 +8,15 @@ PLUGIN_NAME = 'plugins.one'
 # PLUGIN_NAME = 'plugins.two'
 
 plugin_module = importlib.import_module(PLUGIN_NAME, ".")
-
 print(plugin_module)
 
 plugin = plugin_module.Plugin("hello", key=123)
-
 result = plugin.execute(5, 3)
-
 print(result)
+
+# change the module name if needed
+_global = globals()
+_global['alias'] = plugin_module
+
+my_alias = alias.Plugin("alias", key=321)
+print(my_alias.execute(6,5))
