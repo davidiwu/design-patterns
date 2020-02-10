65ff76569dc5332216597c5a15609b33f61bf76d
david <david.iwui@gmail.com>
2020-02-10 10:08:43 +0800

    finish merge sort alogrithm

--- python/sorting/benchmark.py
+++ python/sorting/benchmark.py
@@ -4,6 +4,7 @@ from insertion_sort import *
 from bubble_sort import bubble_sort
 from selection_sort import selection_sort
 from quick_sort import quick_sort
+from merge_sort import merge_sort
 
 size = 10000
 span = 100000
@@ -20,6 +21,7 @@ array_need_sorting = [random.randint(0, span) for _ in range(0, size)]
 copy_array_1 = array_need_sorting.copy()
 copy_array_2 = array_need_sorting.copy()
 copy_array_3 = array_need_sorting.copy()
+copy_array_4 = array_need_sorting.copy()
 
 time1 = timing_sorting(array_need_sorting, insertion_sort)
 
@@ -36,7 +38,10 @@ time4 = timing_sorting(copy_array_2, selection_sort)
 
 time5 = timing_sorting(copy_array_3, quick_sort)
 
+time6 = timing_sorting(copy_array_4, merge_sort)
+
 print(f'insertion sorting time: normal: {time1} ms, worst case: {time1_1}ms, shifting: {time2} ms ')
 print(f'bubble sorting time {time3} ms')
 print(f'selection sorting time {time4} ms')
 print(f'quick sorting time {time5} ms')
+print(f'merge sorting time {time6} ms')
--- python/sorting/merge_sort.py
+++ python/sorting/merge_sort.py
@@ -7,3 +7,48 @@ Merge sort:
     log(n) merge steps because each merge step doubles the list size
     n work for each merge step because it must look at every item
 '''
+
+
+def _split(arr):
+    arr_length = len(arr)
+    idx = arr_length // 2
+    return arr[0:idx], arr[idx:arr_length]
+
+def _merge(arr1, arr2):
+    idx1=0
+    idx2=0
+    ret=[]
+    for _ in range(len(arr1) + len(arr2)):
+        if idx2>=len(arr2) or (idx1 <len(arr1) and arr1[idx1] < arr2[idx2]):
+            ret.append(arr1[idx1])
+            idx1=idx1+1
+        else:
+            ret.append(arr2[idx2])
+            idx2=idx2+1
+
+    return ret
+
+def merge_sort(arr):
+
+    if len(arr) < 2:
+        return arr
+    
+    left, right = _split(arr)
+    merged_left = merge_sort(left)
+    merged_right = merge_sort(right)
+
+    merged = _merge(merged_left, merged_right)
+
+    return merged
+
+if __name__ == '__main__':
+
+    a = [1,3,9,12,16]
+    b = [3,11,17]
+    c = [9,13,22,56]
+
+    print(_merge(a, b))
+    print(_merge(b, c))
+
+    d = [34,1,3,67,11,5,9,23,21,17,4,90,43]
+    print(merge_sort(d))
